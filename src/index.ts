ec060ee819caba2d9946ab0d59250ada07134b92
Mattèo Gauthier <matteo.gauthier@gmail.com>
2023-03-17 10:18:40 +0100

    [API] Prevent score override if lower

--- src/index.ts
+++ src/index.ts
@@ -29,6 +29,16 @@ app.post(
   async (c) => {
     const { email, name, score } = c.req.valid("json")
 
+    const existingScore = await c.env.PLAYER_SCORE.get(email, "text")
+    const leaderboard = await getLeaderBoard(c.env.PLAYER_SCORE)
+
+    if (existingScore && Number(existingScore) > score) {
+      return c.json({
+        message: "Your score is lower than your previous score",
+        leaderboard,
+      })
+    }
+
     await c.env.PLAYER_SCORE.put(email, String(score), {
       metadata: {
         name,
@@ -36,8 +46,6 @@ app.post(
         score,
       } satisfies Metadata,
     })
-
-    const leaderboard = await getLeaderBoard(c.env.PLAYER_SCORE)
 
     return c.json({ message: "Successfully saved score", leaderboard })
   }
